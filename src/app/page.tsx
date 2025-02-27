340f9bac5e8f71cb8662204b590f6512df00f30c
Shreyans Jain <coolshreyans01@gmail.com>
2025-02-27 21:51:45 +0530

    fixed loginButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { LoginButton } from "../../private/loginButton";
+import React from "react";
 
-export default function Home() {
-  return (
-    <>
-      <main className="flex h-full flex-col items-center justify-center bg-gray-950">
-        <LoginButton>
-          <Button variant="secondary">Login</Button>
-        </LoginButton>
-      </main>
-    </>
-  );
-}
+const homePage = () => {
+  return <div>homePage</div>;
+};
+
+export default homePage;
