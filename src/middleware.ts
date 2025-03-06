4b05f1aea596922218e5b35064f66eb655988f63
Shreyans Jain <coolshreyans01@gmail.com>
2025-03-06 13:09:26 +0530

    fixing token: forgot to add

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export async function middleware(req: NextRequest) {
   console.log(token);
 
   if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
+    return NextResponse.next();
   }
 }
 export const config = {
