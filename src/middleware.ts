fc24db7cd93eac9ee2e3ba6084e050c7dd4f2e3b
Shreyans Jain <coolshreyans01@gmail.com>
2025-03-06 13:54:07 +0530

    fixing token: console.log getToken() with secureCookie obj

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,13 +3,17 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(req: NextRequest) {
   console.log("All Cookies in Middleware:", req.cookies.getAll());
 
-  const token = await getToken({ req });
-  console.log("Token in middleware:", token);
+  //   const token = await getToken({ req, raw: true });
+  //   console.log("Token in middleware:", token);
 
-  if (!token) {
-    console.log("No token found, redirecting to login.");
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+  console.log(
+    "Token in middleware: ",
+    await getToken({ req, secureCookie: true })
+  );
+  //   if (!token) {
+  //     console.log("No token found, redirecting to login.");
+  //     return NextResponse.redirect(new URL("/login", req.url));
+  //   }
 
   return NextResponse.next();
 }
