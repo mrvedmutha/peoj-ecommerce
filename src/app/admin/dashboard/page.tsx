35f116817b17c3d9f380ddc882d8d36e09ccad3a
Shreyans Jain <coolshreyans01@gmail.com>
2025-03-06 13:04:15 +0530

    fixing token: getting token manually

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 import React from "react";
 import { signOut } from "next-auth/react";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
 
 const adminDashboard = async () => {
-  const session = await getServerSession(authOptions);
-  console.log("Session from adminDashboard"); //TODO remove
-  console.log(session); //TODO remove
   return (
     <>
       <div>
---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { verify } from "jsonwebtoken";
+import { Roles } from "./types/enum/enumExports";
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("token");
+  console.log("Token in middleware:");
+  console.log(token);
+
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+}
+export const config = {
+  matcher: [
+    "/admin/dashboard",
+    "/cx/dashboard",
+    "/login",
+    "/register",
+    "/verify",
+  ],
+};
